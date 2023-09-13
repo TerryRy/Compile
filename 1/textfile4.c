59e3bb4d37e269041e2f704c08827b2c20f44b5f
TerryRy <2505293361@qq.com>
2023-09-13 13:38:53 +0800

    init

---
+++ 1/textfile4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int getint() {
+    int n = 0;
+    scanf("%d", &n);
+    return n;
+}
+
+
+
+
+
+// Decl不存在
+// 函数不存在
+
+int main() {
+    printf("21371518");
+    return 0;
+}
---
+++ 1/textfile5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int getint() {
+    int n = 0;
+    scanf("%d", &n);
+    return n;
+}
+
+
+
+
+
+// Decl存在1个
+const int const_int_1 = 1;
+// 函数存在1个
+void funcDef() {
+    int n = 0;
+}
+
+int main() {
+    printf("21371518");
+    return 0;
+}
