fb34211e38ac1ddc998311a71a42576224876816
TerryRy <2505293361@qq.com>
2024-08-05 09:05:39 +0800

    文档

---
+++ Compile/build/CMakeFiles/Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Compiler.dir/src/main.cpp.obj"
+  "CMakeFiles/Compiler.dir/src/main.cpp.obj.d"
+  "Compiler.exe"
+  "Compiler.exe.manifest"
+  "Compiler.lib"
+  "Compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
